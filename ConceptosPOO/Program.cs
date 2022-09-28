39b09816d309318de2659f75252d68a5567be8eb
carlos fernandez pradales <cfernande1408@gmail.com>
2022-09-28 16:34:01 +0200

    inicial

---
+++ ConceptosPOO/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConceptosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            realizarTarea();
+        }
+
+        static void realizarTarea()
+        {
+
+            Punto origen = new Punto();
+
+            Punto destino = new Punto(120, 80);
+
+            double distancia = origen.DistanciaHasta(destino);
+
+        }
+    }
+}
